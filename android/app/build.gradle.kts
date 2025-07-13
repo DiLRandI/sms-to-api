43ec1a1a3ffd148f27547207230a37d7f3bc58b9
Deleema Randimal <deleemarf@gmail.com>
2025-07-13 15:16:02 +0530

    fresh start

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.github.dilrandi.sms_to_api"
+    namespace = "com.example.sms_to_api"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.github.dilrandi.sms_to_api"
+        applicationId = "com.example.sms_to_api"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -41,8 +41,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
---
+++ android/app/src/main/kotlin/com/example/sms_to_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sms_to_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
