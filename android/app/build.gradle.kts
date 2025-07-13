1175bbff5fafd13a1b808f8363b5c3cb396d57be
Deleema Randimal <deleemarf@gmail.com>
2025-07-13 15:20:09 +0530

    feat: Update namespace and application ID to align with project structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.sms_to_api"
+    namespace = "com.github.dilrandi.sms_to_api"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sms_to_api"
+        applicationId = "com.github.dilrandi.sms_to_api"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/github/dilrandi/sms_to_api/MainActivity.kt
+++ android/app/src/main/kotlin/com/github/dilrandi/sms_to_api/MainActivity.kt
@@ -1,5 +1,7 @@
-package com.example.sms_to_api
+package com.github.dilrandi.sms_to_api
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
+
+
